15ff7f0d54c405b341ede94413e7bc5276f6cfb7
Chirayu Desai <chirayudesai1@gmail.com>
2018-02-17 02:20:49 +0530

    Square viewfinder

--- app/src/main/java/co/copperhead/attestation/QRScannerActivity.java
+++ app/src/main/java/co/copperhead/attestation/QRScannerActivity.java
@@ -1,8 +1,12 @@
 package co.copperhead.attestation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
@@ -10,6 +14,8 @@ import com.google.zxing.Result;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import me.dm7.barcodescanner.core.IViewFinder;
+import me.dm7.barcodescanner.core.ViewFinderView;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QRScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
@@ -20,8 +26,15 @@ public class QRScannerActivity extends Activity implements ZXingScannerView.Resu
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
+        setContentView(R.layout.activity_qrscanner);
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
+        mScannerView = new ZXingScannerView(this) {
+            @Override
+            protected IViewFinder createViewFinderView(Context context) {
+                return new SquareViewFinderView(context);
+            }
+        };
+        contentFrame.addView(mScannerView);
         mScannerView.setFormats(new ArrayList<BarcodeFormat>(Collections.singletonList(BarcodeFormat.QR_CODE)));
     }
 
@@ -45,5 +58,27 @@ public class QRScannerActivity extends Activity implements ZXingScannerView.Resu
         setResult(Activity.RESULT_OK, result);
         mScannerView.stopCamera();
         finish();
+    }
+
+    private static class SquareViewFinderView extends ViewFinderView {
+        public SquareViewFinderView(Context context) {
+            super(context);
+            init();
+        }
+
+        public SquareViewFinderView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init();
+        }
+
+        private void init() {
+            setSquareViewFinder(true);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+        }
+
     }
 }
